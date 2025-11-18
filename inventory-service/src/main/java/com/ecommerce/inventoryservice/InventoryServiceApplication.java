1ac299435b7b27a7e3b17bd3038384cd2764fd67
Kieuanhdev <kieuanh.dev@gmail.com>
2025-11-18 07:37:25 +0700

    Day 2: Create API Gateway - Single Entry Point for Microservices

--- inventory-service/src/main/java/com/ecommerce/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/ecommerce/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.ecommerce.inventoryservice;
 
+import com.ecommerce.inventoryservice.model.Inventory;
+import com.ecommerce.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,23 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    // 👇 THÊM ĐOẠN NÀY: Tự động chạy khi app khởi động
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            // Chỉ tạo dữ liệu nếu kho đang trống
+            if(inventoryRepository.count() == 0) {
+                Inventory inventory = new Inventory();
+                inventory.setSkuCode("iphone_15");
+                inventory.setQuantity(100);
+
+                Inventory inventory2 = new Inventory();
+                inventory2.setSkuCode("iphone_15_red");
+                inventory2.setQuantity(0);
+
+                inventoryRepository.save(inventory);
+                inventoryRepository.save(inventory2);
+            }
+        };
+    }
 }
